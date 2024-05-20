ebfe1efb37408d9db7c4d68d73c54197acea0ab1
Khalloufi <montassirkhalloufi@gmail.com>
2024-05-20 11:29:37 +0100

    submit test solution

---
+++ src/utils.js
@@ -0,0 +1,12 @@
+/**
+ * Check if a value is an object.
+ * @param {any} value - The value to check.
+ * @return {boolean} - True if the value is an object, otherwise false.
+ */
+function isObject(value) {
+    return typeof value === 'object' && value !== null;
+  }
+  
+  module.exports = {
+    isObject,
+  };
